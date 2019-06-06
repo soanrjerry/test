e741e5628cf626d29840c06bddca709759854469
jerrydevin96 <51227952+jerrydevin96@users.noreply.github.com>
2019-06-06 12:57:45 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,16 @@
+public class qualityCheck {
+  public Static void main(String args[]){
+    int a =10;
+    int b = 20;
+    int c = 30;
+    int d = 40;
+    int sum;
+    sum=a+b;
+    sum=sum+c;
+    sum=sum+d;
+    int count = 100;
+    do{
+      System.out.println("i will print as long as i like";
+    } while (count!=0)
+  }
+}
